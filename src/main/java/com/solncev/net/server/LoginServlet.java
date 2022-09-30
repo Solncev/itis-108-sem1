2eec440b618f11817debb3099ba2c2aede774c2a
maratsolncev <maratsolncev@piano.io>
2022-09-30 11:00:37 +0300

    Filtering and logging samples

--- src/main/java/com/solncev/net/server/LoginServlet.java
+++ src/main/java/com/solncev/net/server/LoginServlet.java
@@ -1,5 +1,8 @@
 package com.solncev.net.server;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
@@ -13,6 +16,7 @@ public class LoginServlet extends HttpServlet {
 
     public static final String LOGIN = "login";
     public static final String PASSWORD = "password123";
+    private static final Logger logger = LoggerFactory.getLogger(LoginServlet.class);
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -25,6 +29,7 @@ public class LoginServlet extends HttpServlet {
         String password = req.getParameter("password");
 
         if (LOGIN.equals(login) && PASSWORD.equals(password)) {
+            logger.info("User with username = {} logged in", login);
             HttpSession httpSession = req.getSession();
             httpSession.setAttribute("username", login);
             httpSession.setMaxInactiveInterval(60 * 60);
--- src/main/webapp/base.ftl
+++ src/main/webapp/base.ftl
@@ -8,6 +8,7 @@
 
 <div id="header">
     <H3> Header </H3>
+    <a href="/logout">Logout</a>
 </div>
 
 <div class="content1">
