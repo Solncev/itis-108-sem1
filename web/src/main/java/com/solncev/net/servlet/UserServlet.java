b68f97b541ee7bdc982321487a987f518f01cb42
maratsolncev <maratsolncev@piano.io>
2022-11-11 10:27:12 +0300

    Separate net and web modules

--- web/src/main/java/com/solncev/net/servlet/UserServlet.java
+++ web/src/main/java/com/solncev/net/servlet/UserServlet.java
@@ -1,6 +1,5 @@
 package com.solncev.net.servlet;
 
-import com.solncev.net.dto.UserDto;
 import com.solncev.net.service.UserService;
 import com.solncev.net.service.impl.UserServiceImpl;
 
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(name = "userServlet", urlPatterns = "/users")
 public class UserServlet extends HttpServlet {
